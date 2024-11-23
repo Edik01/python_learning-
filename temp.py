d4eb5e0402c0d624a6c185907dc2793829a871b1
Edik Huloian <guloyan.edik@gmail.com>
2024-11-23 11:13:52 -0800

    update

--- temp.py
+++ temp.py
@@ -1,3 +1,16 @@
+
+
+amount = float(input("Hi! enter the amount in USD:"))
+currency = input("Enter the target currency (e.g., EUR, CAD, GBP): ").upper()
+try:
+    exchange_rate = c.get_rate('USD', currency)
+    converted_amount = c.convert('USD', currency, amount)
+    print(f"The exchange rate from USD to {currency} is {exchange_rate:.2f}")
+    print(f"{amount} USD is equivalent to {converted_amount:.2f} {currency}.")
+except Exception as e:
+    print(f"An error occurred: {e}")
+
+
 # Current exchange rate from USD to CAD
 exchange_rate = 1.39  # 1 USD = 1.39 CAD
 
@@ -9,3 +22,5 @@ cad_amount = usd_amount * exchange_rate
 
 # Display the result
 print(f"{usd_amount} USD is equivalent to {cad_amount:.2f} CAD.")
+
+
