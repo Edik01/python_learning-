bcf032f03b63cd0b6c58aa5750a25f491fcece5e
Edik Huloian <guloyan.edik@gmail.com>
2024-12-27 21:39:52 -0800

    update

---
+++ loops.py
@@ -0,0 +1,10 @@
+# For loop example
+for i in range(5):
+    print(f"For loop iteration: {i}")
+    # While loop example
+    j = 0
+    while j < 5:
+        print(f"While loop iteration: {j}")
+        j += 1
+
+        
